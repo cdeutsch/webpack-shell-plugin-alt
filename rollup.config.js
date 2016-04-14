9ed07957779e2603ddd1465f788aac05d6ee367c
Patrick Opie <opiepj@plu.edu>
2016-04-14 10:05:49 -0700

    rearrange source good.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'index.js',
+  entry: 'src/webpack-shell-plugin.js',
   format: 'cjs',
   plugins: [babel()],
   dest: 'lib/index.js'
