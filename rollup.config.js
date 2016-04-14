828f75119e52e2dc96bb7cbcc6a8ba3a2d14641e
Patrick Opie <opiepj@plu.edu>
2016-04-14 10:13:00 -0700

    rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/webpack-shell-plugin.js',
   format: 'cjs',
-  plugins: [babel()],
+  plugins: [
+    babel()
+  ],
   dest: 'lib/index.js'
 };
