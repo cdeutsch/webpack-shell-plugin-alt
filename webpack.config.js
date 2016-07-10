96aa0acf04852b7546e73e0118ac6885456695c7
Patrick Opie <opiepj@plu.edu>
2016-07-09 17:46:50 -0700

    test files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+var WebpackShellPlugin = require('./lib');
+
+module.exports = {
+  watch: true,
+  entry: path.resolve(__dirname, 'test/entry.js'),
+  output: {
+    path: path.resolve(__dirname, 'test'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'test')
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  plugins: [
+    new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:['echo "Webpack End"']}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
